2b2546e5a4e68770752b96d9a022597c404e2891
Johann Alberts <alberts.johann@gmail.com>
2021-02-07 17:33:10 +0000

    Add model class using enums

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
+from enum import Enum
 from typing import Optional
 from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
@@ -12,3 +20,14 @@ def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
